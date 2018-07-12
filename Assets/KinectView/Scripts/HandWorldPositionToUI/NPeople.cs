8bb5d837ee874e5e58bc6ca3f875591dde22fad0
zhouy546 <zhouyunqi546@gmail.com>
2018-07-12 17:29:34 +0800

    Initial commit

---
+++ Assets/KinectView/Scripts/HandWorldPositionToUI/NPeople.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPeople : MonoBehaviour {
+
+    private Vector3 leftHandPos;
+    public Vector3 LeftHandPos {
+        get {
+            return leftHandPos;
+        }
+        set {
+            leftHandPos= value;
+        }
+    }
+
+    private Vector3 rightHandPos;
+    public Vector3 RightHandPos {
+        get
+        {
+            return rightHandPos;
+        }
+        set
+        {
+            rightHandPos = value;
+        }
+    }
+
+    public Vector3 CenterPos;
+    public float xMaxValue=6, xMinValue=-6;
+    public float yMaxValue = 6, yMinValue=-4;
+
+
+    public Vector3 MappedLeftHandPos {
+
+        get {
+            float x = Mapping(LeftHandPos.x, xMinValue, xMaxValue, -1920F / 2F, 1920F / 2F);
+            float y = Mapping(LeftHandPos.y, yMinValue, yMaxValue, -1200 / 2F, 1200 / 2F);
+            return new Vector3(x, y, 0);
+        }
+    }
+
+    public Vector3 MappedRightHandPos
+    {
+
+        get
+        {
+            float x = Mapping(RightHandPos.x, xMinValue, xMaxValue, -1920F / 2F, 1920F / 2F);
+            float y = Mapping(RightHandPos.y, yMinValue, yMaxValue, -1200 / 2F, 1200 / 2F);
+            return new Vector3(x, y, 0);
+        }
+    }
+
+    public NPeople() {
+
+    }
+
+
+    float Mapping(float value, float inputMin, float inputMax, float outputMin, float outputMax)
+    {
+
+        float outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+
+        return outVal;
+    }
+}
---
+++ Assets/KinectView/Scripts/HandWorldPositionToUI/UpdateUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kinect = Windows.Kinect;
+
+public class UpdateUI : MonoBehaviour {
+    public BodySourceView bodySourceView;
+
+    public GameObject Cursor;
+    public List<GameObject> cursors = new List<GameObject>();
+
+    int num = 0;
+    // Use this for initialization
+    void Start () {
+
+        bodySourceView = FindObjectOfType<BodySourceView>();
+
+        CreateCurser();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+   public void updateCursors(ulong[] id) {
+
+
+        for (int i = 0; i<id.Length;  i++)
+        {
+
+            if (bodySourceView._Npeoples.ContainsKey(id[i])) {
+                cursors[2 * i].transform.localPosition = bodySourceView._Npeoples[id[i]].MappedRightHandPos;
+
+
+                cursors[2 * i + 1].transform.localPosition = bodySourceView._Npeoples[id[i]].MappedLeftHandPos;
+
+            }
+        }
+    }
+
+
+    void CreateCurser() {
+        for (int i = 0; i < 14; i++)
+        {
+            GameObject _Cursor= Instantiate(Cursor);
+            _Cursor.transform.SetParent(this.transform);
+            _Cursor.transform.position = new Vector3(-10000, -10000, 0);
+            cursors.Add(_Cursor);
+        }
+    }
+}
